11710c16044070e1117bd75fb73874a1aa0f3528
janluyleonardo <janluy.moreno@gmail.com>
2026-03-12 15:15:02 -0500

    ajustes de dirver actualizado para conexion a sevicor de mysql 8

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -5,15 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    private final String base = "tienda";
+    private final String base = "tiendita_veci";
     private final String url = "jdbc:mysql://127.0.0.1/" + base+"?characterEncoding=latin1";
-    private final String user = "root";
-    private final String pass = "Moreno880501";
+    private final String user = "MORJANDEV";
+    private final String pass = "Morjan*1025544889";
     private Connection con = null;
 
     public Connection getConexion() {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
 //            con = (Connection) DriverManager.getConnection(url, user, pass);
             con = (Connection) DriverManager.getConnection(url,user,pass);
             System.out.println("conexion exitosa a:" + base);
